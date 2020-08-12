3a0a62ae1c1d7394dfb73f975c746c2accb5148e
HEEJAE <heejae.tech@gmail.com>
2020-08-12 10:48:08 +0900

    footer page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// The Footer
+export default function Footer() {
+  return (
+    <footer className="App-footer">
+      <span className="footer_description">
+        Implemented by Heejae Lee
+      </span>
+    </footer>
+  );
+}
